f5771dd881bcdc7cfbaef2e0b8c4674e6945aa63
lion-oldriver <w879443a72cj2i@gmail.com>
2021-08-10 09:51:26 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  devise_for :users
+  root to:"homes#top" 
+  resources :books
+  resources :users, only:[:index,:show,:edit,:update]
 end
