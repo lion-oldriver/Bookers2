e212e1bdbf7047a96f0c2d07b19f3aaba2a49b16
lion-oldriver <w879443a72cj2i@gmail.com>
2021-08-19 07:24:14 +0000

    [update]follow/follower

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,25 @@ class User < ApplicationRecord
   has_many :books,dependent: :destroy
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
-  
+  has_many :relationships, foreign_key: 'user_id'
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
   validates :name,length: { in: 2..20 }
   validates :name,uniqueness: true
   validates :introduction,length: { maximum: 50 }
+
+  def follow(user_id)
+    relationships.create(follow_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(follow_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
